6eafdc3156cc915dd5c1caaca6a5bfb68518f775
Kevin Boyle <kevin.boyle@outlook.com>
2015-02-24 11:32:59 +0000

    Prevent players from entering duplicate key bindings

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/UserInterface.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/UserInterface.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Mail;
 
 namespace ConsoleBasedGame
 {
@@ -7,13 +8,19 @@ namespace ConsoleBasedGame
     {
         public IEnumerable<PlayerInfo> GetPlayerInfoFromUserLazily()
         {
+            HashSet<char> invalidKeys = new HashSet<char>();
             bool again;
             do
             {
                 Console.Write("Enter player name: ");
                 var playerName = Console.ReadLine();
-                var playCardKey = AskForKey("Enter play card key: ");
-                var snapKey = AskForKey("Enter snap key: ");
+
+                var playCardKey = AskForKey("Enter play card key: ", invalidKeys);
+                invalidKeys.Add(playCardKey);
+
+                var snapKey = AskForKey("Enter snap key: ", invalidKeys);
+                invalidKeys.Add(snapKey);
+
                 yield return new PlayerInfo(playerName, playCardKey, snapKey);
 
                 var createPlayerKey = AskForKey("Create another player? (y|n): ");
@@ -21,10 +28,15 @@ namespace ConsoleBasedGame
             } while (again);
         }
 
-        private static char AskForKey(string prompt)
+        private static char AskForKey(string prompt, HashSet<char> invalidKeys = null)
         {
             Console.Write(prompt);
             var response = Console.ReadKey().KeyChar;
+            while (invalidKeys != null && invalidKeys.Contains(response))
+            {
+                Console.WriteLine("Already used, " + prompt);
+                response = Console.ReadKey().KeyChar;
+            }
             Console.WriteLine();
             return response;
         }
