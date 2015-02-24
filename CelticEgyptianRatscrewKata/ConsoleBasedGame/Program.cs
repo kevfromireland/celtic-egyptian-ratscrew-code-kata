b704a86dcc502482cae81979159c6f4dbcc06aa3
Kevin Boyle <kevin.boyle@outlook.com>
2015-02-24 11:17:10 +0000

    Some bad code to get us started

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/Program.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CelticEgyptianRatscrewKata.Game;
 
 namespace ConsoleBasedGame
@@ -10,7 +12,7 @@ namespace ConsoleBasedGame
             GameController game = new GameFactory().Create();
 
             var userInterface = new UserInterface();
-            IEnumerable<PlayerInfo> playerInfos = userInterface.GetPlayerInfoFromUserLazily();
+            IEnumerable<PlayerInfo> playerInfos = userInterface.GetPlayerInfoFromUserLazily().ToList();
 
             foreach (PlayerInfo playerInfo in playerInfos)
             {
@@ -22,8 +24,9 @@ namespace ConsoleBasedGame
             char userInput;
             while (userInterface.TryReadUserInput(out userInput))
             {
-
-            } 
+                if (!playerInfos.Any(info => info.PlayCardKey == userInput || info.SnapKey == userInput))
+                    Console.WriteLine("Invalid input");
+            }
         }
     }
 }
